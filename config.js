18db4210d917b84966457d9b650d20f317df7a3b
Kamil Wojtczyk <kamil@gearedapp.co.uk>
2019-07-05 11:40:17 +0100

    first commit :tada:

---
+++ config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  // the name of your website
+  title: 'Kamil Wojtczyk',
+  // the description of the website (eg. what shows on Google)
+  description: "Kamil Wojtczyk's blog",
+  // a short bio shown at the bottom of your blog posts
+  // It should complete the sentence: Written by Kamil Wojtczyk ...
+  shortBio: '',
+  // a longer bio showing on the landing page of the blog
+  bio: ``,
+  author: 'Kamil Wojtczyk',
+  githubUrl: 'https://github.com/k0ff33/blog-test',
+  // replace this by the url where your website will be published
+  siteUrl: 'http://localhost:8000',
+  social: {
+    // leave the social media you do not want to appear as empty strings
+    twitter: '',
+    medium: '@k.wojtczyk',
+    facebook: '',
+    github: '',
+    linkedin: '',
+    instagram: '',
+  },
+}
